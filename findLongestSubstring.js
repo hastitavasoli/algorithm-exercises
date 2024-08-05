fca099c19881683580c64d3e71e954f1749819c4
hasti tavasoli <hasti_tavassoli@yahoo.com>
2024-08-05 13:17:18 +0330

    findLongestSubstring

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -13,6 +13,34 @@ findLongestSubstring('thisishowwedoit') // 6
 Time Complexity - O(n)
 **/
 
-function findLongestSubstring(){
-  // add whatever parameters you deem necessary - good luck!
+function findLongestSubstring(mainString) {
+  let indexOfChar = {}
+  let subString = ''
+  let index = 0
+
+  if (typeof mainString == 'string' && mainString.length > 1) {
+    while (mainString.length > 0) {
+      if (typeof indexOfChar[mainString[index]] != 'undefined') {
+        let tmpString = mainString.slice(0, index)
+        if (tmpString.length > subString.length) {
+          subString = tmpString
+        }
+        mainString = mainString.slice(indexOfChar[mainString[index]] + 1)
+        indexOfChar = {}
+        index = 0
+      } else {
+        indexOfChar[mainString[index]] = index
+        if (index == (mainString.length - 1)) {
+          let tmpString = mainString
+          mainString = ''
+          if (tmpString.length > subString.length) {
+            subString = tmpString
+          }
+        } else {
+          index = index + 1
+        }
+      }
+    }
+  }
+  return (subString.length)
 }
