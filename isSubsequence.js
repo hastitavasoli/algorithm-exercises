c9f6475b0c4fe34ff4c784325ded95df37569a96
hasti tavasoli <hasti_tavassoli@yahoo.com>
2024-08-05 16:53:42 +0330

    isSubsequence

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,5 +1,5 @@
 /** 
-Multiple Pointers - isSubsequence
+ 
 Write a function called is Subsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
 
 Examples:
@@ -15,6 +15,25 @@ Space Complexity - 0(1)
 
 **/
 
-function isSubsequence() {
-  // good luck. Add any arguments you deem necessary.
+function isSubsequence(subString, mainString) {
+  let subIndex = 0
+  let mainIndex = 0
+
+  if (subString.length < mainString.length) {
+    while (subIndex < subString.length && mainIndex < mainString.length) {
+      if (subString[subIndex] === mainString[mainIndex]) {
+        subIndex = subIndex + 1
+      }
+      mainIndex = mainIndex + 1
+    }
+    if (subIndex === subString.length) {
+      return true
+    }
+
+    return false
+
+  } else {
+
+    return false
+  }
 }
